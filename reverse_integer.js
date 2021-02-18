b1d15e993da0f212edcc0480110aee9fa4069cad
shakir-uz-zaman <shakir.uz.zaman.shuvo@gmail.com>
2021-02-18 15:40:40 +0600

    reverse integer

---
+++ reverse_integer.js
@@ -0,0 +1,26 @@
+// 123= 321
+// 120 = 21
+// -3456 =-6543
+
+function reverse(x)
+{   
+    let res=0;
+    let position=0;
+    let base_value=10;
+    let negative = false;
+    if(x<0)
+    {
+        negative =true ; 
+        x= 0-x;
+    }
+    if(x==0)
+    {
+        return x;
+    }
+    
+
+    return x;
+    
+}
+
+console.log(reverse(530));
